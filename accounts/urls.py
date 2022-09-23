b25d5722f67da9764e649af0b570ac4839fa7458
donghyeok1 <07385@naver.com>
2022-09-23 20:08:05 +0900

    로그인,로그아웃, 회원가입 자동 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
+    path('login/', views.login, name='login'), # /accounts/login/ => settings.LOGIN_URL
+    # 최상위 루트 페이지는 loginrequired가 적용이 되어있어서 admin에서 로그아웃을 시키고
+    # 루트페이지로 가보면 accounts/login url을 따라가게됨
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,31 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.views import LoginView, logout_then_login
 from accounts.forms import SignupForm
 
 
-# Create your views here.
+login = LoginView.as_view(template_name="accounts/login_form.html")
+# 로그인 ui를 바꾸고 싶다면 getbootsrap 페이지로 가서 documentation에 card 레이아웃을 찾아라
 
+def logout(request):
+    messages.success(request, "로그아웃되었습니다.")
+    return logout_then_login(request)
+# https://github.com/django/django/blob/main/django/contrib/auth/views.py
+# logout_then_login 함수가 구현이 되어있음!
+# logout을 한다면 바로 login 페이지로 보내버림!
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user)
+            # from django.contrib.auth import login as auth_login
+            # github에 가보면 auth의 __init__.py에 login 함수가 있다.
+            # login을 import 하는데 login은 겹칠 수 있으니 auth_login으로 이름을 바꾼다
+            # 이 함수는 회원가입하자마자 로그인하게 해주는 함수이다.
+            # 이걸 안쓰면 장고단에서 있는 user 모델을 바로 불러올 수 없다.
             messages.success(request, "회원가입 환영합니다.")
             # 이 메세지는 html에서 노출을 시키지 않으면 계속 쌓이게 됨.
             # layout.html에서 구현
@@ -25,3 +39,6 @@ def signup(request):
     return render(request, 'accounts/signup_form.html',{
         'form' : form,
     })
+
+# https://sendgrid.com/docs/for-developers/sending-email/django/
+# 회원 가입 email 보내기 send_mail API(SMTP)
