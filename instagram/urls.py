9794e6a335bcc15387d4bee4fac0a259d1451cd7
donghyeok1 <07385@naver.com>
2022-09-24 03:48:00 +0900

    username을 url로 해서 인스타 자기 게시물 보기

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,10 +1,26 @@
-from django.urls import path
-
+from django.urls import path, re_path
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from instagram import views
 
 app_name = 'instagram'
 
+#     username을 url로 쓰기 위해서
+#     https://github.com/django/django/blob/main/django/contrib/auth/models.py에 들어가봣는데
+#     username을 찾아보니 username_validator = UnicodeUsernameValidator()를 쓴다
+#     UnicodeUsernameValidator()를 따라 들어가보면
+# class UnicodeUsernameValidator(validators.RegexValidator):
+#     regex = r"^[\w.@+-]+\Z"
+#     message = _(
+#         "Enter a valid username. This value may contain only letters, "
+#         "numbers, and @/./+/-/_ characters."
+#     )
+#     flags = 0
+#     이런식으로 구현되어 있다.
+
 urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    re_path(r'^(?P<username>[\w.@+-]+)/$', views.user_page, name='user_page'),
 ]
+#   이렇게 되면 username이 post라면 위에서 걸러지게 되서 밑에까지 못오게 됨.
+#   그러므로 model에 가서 username의 제한을 걸어줘야한다.
